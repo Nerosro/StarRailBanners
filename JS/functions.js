c7f562a0e3200a3424d530bc64897ab55500f2e4
Nerosro <5601152+Nerosro@users.noreply.github.com>
2024-11-23 23:05:04 +0100

    Fix image location + Update CSS
    	modified:   index.html

--- JS/functions.js
+++ JS/functions.js
@@ -3,16 +3,17 @@ function createGallery() {
 }
 
 function createCharacterCard(characterName) {
+    const root = document.getElementById("root");
     const loc_5star = "images/characters/5_stars/" + characterName + ".webp";
 
     let topDiv = document.createElement("div");
     let img = document.createElement("img");
     img.src=loc_5star;
     img.alt=characterName;
-    img.x=140*2;
-    img.y=189*2;
-    img.className=".rarity-5"
+    img.width=140*2;
+    img.height=189*2;
+    img.className=".rarity-5";
 
     topDiv.append(img);
-    return topDiv
+    root.append(topDiv);
 }
