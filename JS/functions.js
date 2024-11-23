406276a3f6927f3e8f5a99e27f6b421e139456f5
Nerosro <5601152+Nerosro@users.noreply.github.com>
2024-11-23 23:14:19 +0100

    Switch to gallery and loop over array instead of manually adding characters

--- JS/functions.js
+++ JS/functions.js
@@ -1,19 +1,25 @@
 function createGallery() {
 
+    const characterArray = ["Firefly", "Sparkle", "Agaea"]
+    let topDiv = document.createElement("div");
+    topDiv.id="root"
+
+    for(const character in characterArray) {
+        topDiv.append(createCharacterCard(characterArray));
+    }
+
+    return topDiv;
 }
 
 function createCharacterCard(characterName) {
-    const root = document.getElementById("root");
     const loc_5star = "images/characters/5_stars/" + characterName + ".webp";
 
-    let topDiv = document.createElement("div");
     let img = document.createElement("img");
     img.src=loc_5star;
     img.alt=characterName;
     img.width=140*2;
     img.height=189*2;
-    img.className=".rarity-5";
+    img.className="rarity-5";
 
-    topDiv.append(img);
-    root.append(topDiv);
+    return img;
 }
