e1b56ab578f43e426f1b519dff1b834bb22062d2
Nerosro <5601152+Nerosro@users.noreply.github.com>
2024-11-23 23:45:34 +0100

    Improve CSS

--- JS/functions.js
+++ JS/functions.js
@@ -1,25 +1,32 @@
 function createGallery() {
 
+    const topNode = document.getElementsByClassName("content")[0]
     const characterArray = ["Firefly", "Sparkle", "Agaea"]
-    let topDiv = document.createElement("div");
-    topDiv.id="root"
+    let rootDiv = document.createElement("div");
+    rootDiv.id = "root card";
 
-    for(const character in characterArray) {
-        topDiv.append(createCharacterCard(characterArray));
+    for (const character in characterArray) {
+        const span = document.createElement("span")
+        span.append(
+            createCharacterCard(characterArray[character])
+        );
+        rootDiv.append(span)
     }
 
-    return topDiv;
+    topNode.append(rootDiv);
 }
 
 function createCharacterCard(characterName) {
     const loc_5star = "images/characters/5_stars/" + characterName + ".webp";
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "rarity-5";
 
     let img = document.createElement("img");
-    img.src=loc_5star;
-    img.alt=characterName;
-    img.width=140*2;
-    img.height=189*2;
-    img.className="rarity-5";
+    img.src = loc_5star;
+    img.alt = characterName;
+    img.width = 140 * 2;
+    img.height = 189 * 2;
 
-    return img;
+    imgDiv.append(img);
+    return imgDiv;
 }
