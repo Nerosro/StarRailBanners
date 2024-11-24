31e8a1c3093def436d6da1165d59a92dc1ba8a9f
Nerosro <5601152+Nerosro@users.noreply.github.com>
2024-11-24 02:57:05 +0100

    Showing all banners

--- JS/functions.js
+++ JS/functions.js
@@ -1,18 +1,27 @@
-function createGallery() {
-
+function createGallery(jsonData) {
     const topNode = document.getElementsByClassName("content")[0]
-    const characterArray = ["Firefly", "Sparkle", "Agaea"]
     let rootDiv = document.createElement("div");
-    rootDiv.id = "root card";
+    rootDiv.id = "root";
+    rootDiv.className = "card"
 
-    for (const character in characterArray) {
-        const span = document.createElement("span")
-        span.append(
-            createCharacterCard(characterArray[character])
-        );
-        rootDiv.append(span)
+    const characterData = parseJSONObject(jsonData)
+
+    let majorVersionNumber = Object.keys(characterData);
+
+    for (let i = 0; i < majorVersionNumber.length; i++) {
+        let characters = characterData[majorVersionNumber[i]];
+        let versionDiv = document.createElement("div");
+        versionDiv.id = "version_" + majorVersionNumber[i];
+
+        for (let j = 0; j < characters.length; j++) {
+            const span = document.createElement("span")
+            span.append(
+                createCharacterCard(characters[j])
+            );
+            versionDiv.append(span)
+        }
+        rootDiv.append(versionDiv);
     }
-
     topNode.append(rootDiv);
 }
 
@@ -29,4 +38,42 @@ function createCharacterCard(characterName) {
 
     imgDiv.append(img);
     return imgDiv;
+}
+
+/*Will return an object that looks like below
+        {
+            "1":[
+                character1,
+                character2,
+                character3,
+                etc
+            ],
+            "2":[
+                character1,
+                character2,
+                character3,
+                etc
+            ]
+        }
+    */
+function parseJSONObject(json) {
+    let versionObject = {};
+    for (let i = 0; i < json.bannerHistory.length; i++) {
+        let majorVersion = json.bannerHistory[i].version.charAt(0);
+        if (versionObject[majorVersion] == null || versionObject[majorVersion] === undefined)
+            versionObject[majorVersion] = [];
+
+        let characters = json.bannerHistory[i].characters;
+
+        for (let j = 0; j < characters.length; j++) {
+            if (characters[j].featured !== undefined && characters[j].featured != null) {
+                versionObject[majorVersion].push(characters[j].featured)
+            }
+            // let rerun = characters[j].rerun;
+            // if (rerun != undefined && rerun != null){
+            //     versionObject[majorVersion].push(...rerun);
+            // }
+        }
+    }
+    return versionObject;
 }
