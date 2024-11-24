d0b14669470523444cc6b6e8dfa4c09975c7968f
Nerosro <5601152+Nerosro@users.noreply.github.com>
2024-11-24 18:40:22 +0100

    Rework javascript to create sections Update CSS to reflect changes

--- JS/functions.js
+++ JS/functions.js
@@ -1,71 +1,104 @@
 function createGallery(jsonData) {
     const topNode = document.getElementsByClassName("content")[0]
     let rootDiv = document.createElement("div");
-    rootDiv.id = "root";
-    rootDiv.className = "card"
+    rootDiv.className = "root"
 
-    const characterData = parseJSONObject(jsonData)
+    //console.log("minor version: " + jsonData[0]["banners"][0]["minor_version"])
+    //console.log(jsonData[0]["banners"][3]["characters"][0]["featured"])
 
-    let majorVersionNumber = Object.keys(characterData);
+    createVersionDiv(jsonData, rootDiv);
 
-    for (let i = 0; i < majorVersionNumber.length; i++) {
-        let characters = characterData[majorVersionNumber[i]];
-        let majorVersionDiv = document.createElement("div");
-        majorVersionDiv.className = "version_" + majorVersionNumber[i];
-
-        for (let j = 0; j < characters.length; j++) {
-            const span = document.createElement("span")
-            span.append(
-                createCharacterCard(characters[j])
-            );
-            majorVersionDiv.append(span)
-        }
-        rootDiv.append(majorVersionDiv);
-    }
-    topNode.append(rootDiv);
+    topNode.append(rootDiv)
 }
 
-function createCharacterCard(characterName) {
+function createVersionDiv(jsonData, rootDiv) {
+    for (const entry of jsonData) {
+
+        const majorVersionNumber = entry["major_version"];
+        const banners = entry["banners"];
+
+        const version = "version-" + majorVersionNumber + "x";
+        //console.log("major version: " + version)
+
+        let versionDiv = document.createElement("div");
+        versionDiv.id = version;
+        versionDiv.className = "major";
+
+        createBannerData(banners, majorVersionNumber, versionDiv);
+
+        rootDiv.append(versionDiv);
+    }
+}
+
+function createBannerData(banners, majorVersionNumber, versionDiv) {
+    for (const entry of banners) {
+        let minorDiv = document.createElement("div");
+        minorDiv.id = "version-" + majorVersionNumber + "_" + entry["minor_version"];
+        minorDiv.className = "minor";
+        console.info("version-" + majorVersionNumber + "_" + entry["minor_version"])
+
+        createCharacterData(entry, minorDiv);
+
+        versionDiv.append(minorDiv);
+    }
+}
+
+function createCharacterData(entry, minorDiv) {
+    for (const characters of entry["characters"]) {
+        const featuredCharacter = characters["featured"]
+        const rerunCharacter = characters["rerun"]
+        const fourStarCharacter = characters["featured4"]
+
+        const featuredDiv = document.createElement("div");
+        featuredDiv.className = "character featured rarity-5";
+        const rerunDiv = document.createElement("div");
+        rerunDiv.className = "character rerun rarity-5";
+
+        const fourStarDiv = document.createElement("div");
+        fourStarDiv.className = "character rarity-4";
+
+        if (featuredCharacter != null) {
+            featuredDiv.append(createCharacterCard(featuredCharacter, 5));
+            minorDiv.append(featuredDiv)
+        }
+
+        if (rerunCharacter != null) {
+            for (const character of rerunCharacter) {
+                console.info(character)
+                rerunDiv.append(createCharacterCard(character, 5));
+            }
+            minorDiv.append(rerunDiv)
+        }
+
+        if (fourStarCharacter != null) {
+            for (const character of fourStarCharacter) {
+                //console.info(character)
+                //fourStarDiv.append(createCharacterCard(character, 4))
+                minorDiv.append(fourStarDiv)
+            }
+        }
+    }
+}
+
+function createCharacterCard(characterName, rarity) {
     const loc_5star = "images/characters/5_stars/" + characterName + ".webp";
+    const loc_4star = "images/characters/4_stars/" + characterName + ".webp";
     const imgDiv = document.createElement("div");
-    imgDiv.className = "rarity-5";
+    imgDiv.className = "rarity-" + rarity;
 
     let img = document.createElement("img");
-    img.src = loc_5star;
+    if (rarity === 4) {
+        img.src = loc_4star;
+    } else if (rarity === 5) {
+        img.src = loc_5star;
+    } else {
+        console.log(rarity);
+    }
+
     img.alt = characterName;
     img.width = 140 * 2;
     img.height = 189 * 2;
 
     imgDiv.append(img);
     return imgDiv;
-}
-
-function parseJSONObject(json) {
-    let versionObject = {};
-    for (let i = 0; i < json.bannerHistory.length; i++) {
-        let majorVersion = json.bannerHistory[i].version.charAt(0);
-        if (versionObject[majorVersion] == null || versionObject[majorVersion] === undefined)
-            versionObject[majorVersion] = [];
-
-        let characters = json.bannerHistory[i].characters;
-
-        for (let j = 0; j < characters.length; j++) {
-            if (characters[j].featured !== undefined && characters[j].featured != null) {
-                versionObject[majorVersion].push(characters[j].featured)
-            }
-
-            let rerun = characters[j].rerun;
-            if (rerun !== undefined && rerun != null){
-                if(Array.isArray(rerun)){
-                    for(let k = 0; k < rerun.length; k++){
-                        versionObject[majorVersion].push(rerun[k]);
-                    }
-                }
-                else{
-                    versionObject[majorVersion].push(rerun);
-                }
-            }
-        }
-    }
-    return versionObject;
 }
